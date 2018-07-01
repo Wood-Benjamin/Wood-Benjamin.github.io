185a0d388801773fa6f7a06b967566bd5c77629a
Wood-Benjamin <38670310+Wood-Benjamin@users.noreply.github.com>
2018-06-30 21:15:36 -0600

    bw

--- assignments/weathersite/scripts/currentdate.js
+++ assignments/weathersite/scripts/currentdate.js
@@ -1,4 +1,3 @@
-
 var d = new Date();
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
